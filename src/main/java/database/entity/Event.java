eb7957f60455479ee6c9c335ddaa24230dd4f8f7
Mateusz Ligus <mateusz.ligus@iteo.com>
2018-11-06 12:36:22 +0100

    create event categories, if they do not exists, prepare endpoint for categories

---
+++ src/main/java/database/entity/Event.java
@@ -0,0 +1,151 @@
+package database.entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class Event {
+    private String title;
+    private User owner;
+    private List<User> participants;
+    private String description;
+    private String photo;
+    private Long latitude;
+    private Long longitude;
+    private Date startDate;
+    private Date endDate;
+    private boolean showGuestList;
+    private int maxParticipants;
+    private boolean onlyRegistered;
+    private List<Category> categories;
+    private List<Tag> tags;
+    private Long cost;
+    private String externalUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Long latitude) {
+        this.latitude = latitude;
+    }
+
+    public Long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Long longitude) {
+        this.longitude = longitude;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isShowGuestList() {
+        return showGuestList;
+    }
+
+    public void setShowGuestList(boolean showGuestList) {
+        this.showGuestList = showGuestList;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
+
+    public boolean isOnlyRegistered() {
+        return onlyRegistered;
+    }
+
+    public void setOnlyRegistered(boolean onlyRegistered) {
+        this.onlyRegistered = onlyRegistered;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Long getCost() {
+        return cost;
+    }
+
+    public void setCost(Long cost) {
+        this.cost = cost;
+    }
+
+    public String getExternalUrl() {
+        return externalUrl;
+    }
+
+    public void setExternalUrl(String externalUrl) {
+        this.externalUrl = externalUrl;
+    }
+}
