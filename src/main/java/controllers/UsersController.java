72d2e87e659310aeb2190fb3eac2a8b6971f21e2
lothar90 <rafalswoboda@wp.pl>
2019-01-11 12:53:14 +0100

    Added user and user's events removal. TODO: remove token, so user stops being authorized

--- src/main/java/controllers/UsersController.java
+++ src/main/java/controllers/UsersController.java
@@ -7,16 +7,17 @@ import com.mongodb.client.MongoDatabase;
 import database.DatabaseConnection;
 import database.entity.Event;
 import database.entity.User;
+import helpers.Authorization;
 import helpers.DateHelper;
+import helpers.KeyDecoder;
 import model.ApiException;
 import model.EventShortDataDto;
 import model.UserProfileDto;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -56,5 +57,27 @@ public class UsersController {
                 upcomingUserEvents.add(new EventShortDataDto(event, user.getNickname(), true));
 
         return Response.ok().entity(gson.toJson(new UserProfileDto(user, upcomingUserEvents, userEvents))).build();
+    }
+
+    @DELETE
+    @Produces("application/json")
+    public Response deleteUser(@Context HttpServletRequest request) {
+        //TODO: remove token
+        if (Authorization.shared.isAuthenticated(request).getStatusCode() != 200) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity(gson.toJson(new ApiException("User authorization failed"))).build();
+        }
+        MongoCollection<Event> events = database.getCollection("Events", Event.class);
+        MongoCollection<User> users = database.getCollection("Users", User.class);
+        String email = KeyDecoder.shared.decode(request);
+        User existingUser = users.find(eq("email", email)).first();
+        if(existingUser == null)
+            return Response.status(Response.Status.BAD_REQUEST).entity(gson.toJson(new ApiException("User not found"))).build();
+
+        FindIterable<Event> userEvents = events.find(eq("ownerId", existingUser.getId()));
+        for (Event e : userEvents) {
+            events.deleteOne(new Document("_id", e.getId()));
+        }
+        users.deleteOne(new Document("_id", existingUser.getId()));
+        return Response.ok().build();
     }
 }
