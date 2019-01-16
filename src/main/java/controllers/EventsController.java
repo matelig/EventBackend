6cd9aa3e517a66490614c01d32529ac53da98279
matelig <ligus.mateusz@gmail.com>
2019-01-16 16:20:31 +0100

    return 10 events

--- src/main/java/controllers/EventsController.java
+++ src/main/java/controllers/EventsController.java
@@ -171,7 +171,7 @@ public class EventsController {
                 resultDtos.add(esdd);
                 i++;
             }
-            if(i == 5) {
+            if(i == 10) {
                 break;
             }
         }
